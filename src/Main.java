626d6250518ca1955bbb39929283e1a540e67920
Shkil <lyuda.shckil@yandex.ru>
2022-08-27 13:01:28 +0300

    1

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int cost = 2000;
+        int amount = 20;
+        int bonus = (cost) / 20;
+        if ((cost) > 20) {
+            System.out.println("Ваш бонус :" + bonus);
+
+
+        }
+
+    }
+}
